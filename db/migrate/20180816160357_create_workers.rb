0979fed136b0262f45fdc9d37f499deb3b0b27e6
ep <epedrotti@indeep.ai>
2018-08-16 18:18:37 +0200

    fist commit

---
+++ db/migrate/20180816160357_create_workers.rb
@@ -0,0 +1,11 @@
+class CreateWorkers < ActiveRecord::Migration[5.2]
+  def change
+    create_table :workers do |t|
+      t.string :fist_name
+      t.string :last_name
+      t.integer :age
+
+      t.timestamps
+    end
+  end
+end
